202f3a603da8702efda5906c972ef5eaa61a55b2
Wellington Ivo <wellingtonivoprog@gmail.com>
2022-08-17 14:38:06 -0300

    finish app. v.1.0.0

---
+++ src/components/Credits.js
@@ -0,0 +1,13 @@
+import "./Credits.css"
+
+export default function Credits() {
+  return (
+    <div>
+        <p className="credits">
+            This application was made with the guidance of Net Ninja's React course "Build Web Apps with React &amp; Firebase". You can check it by clicking <a 
+            className="credits-link"
+            href="https://www.udemy.com/course/build-web-apps-with-react-firebase/" target="_blank">HERE</a>
+            </p>
+      </div>
+  )
+}
